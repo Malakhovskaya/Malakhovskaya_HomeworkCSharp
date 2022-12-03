dbcf35ed27e62022b5a3010aeb6d6004bd7e9377
Your Name <trandina.e@gmail.com>
2022-12-03 19:44:42 +0300

    add Example005 project

---
+++ Example005_Random/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = new Random().Next(1, 10);
+Console.Write("Первое число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.Write("Второе число: ");
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write("Результат суммы: ");
+Console.WriteLine(result);
